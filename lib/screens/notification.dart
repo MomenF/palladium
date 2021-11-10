fb90faf51aea0858eadd315ac46e7fc98004496d
MomenF <86335146+MomenF@users.noreply.github.com>
2021-11-10 16:13:46 +0200

    adding Subscribe

---
+++ lib/screens/notification.dart
@@ -0,0 +1,222 @@
+import 'package:flutter/material.dart';
+
+import '../config.dart';
+
+
+class NotificationScreen extends StatefulWidget {
+  @override
+  _NotificationScreenState createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backGroundApp,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back,color: Colors.white,),
+          onPressed: (){
+            Navigator.pop(context);
+          },
+        ),
+        centerTitle: true,
+        title: Text(
+          'Notifications',
+          style: TextStyle(
+            fontFamily: 'Open Sans',
+            fontSize: 20,
+            color: const Color(0xff199be0),
+            letterSpacing: 0.8,
+          ),
+
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search,color: Colors.white,),
+            onPressed: (){},
+          ),
+        ],
+      ),
+      backgroundColor: backGroundApp,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white.withOpacity(0.0),
+        onPressed: () {
+          // changeIndexScreen(context, 2);
+        },
+        child: Container(
+          height: 56,
+          width: 56,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            gradient: const LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.lightBlueAccent,
+                Colors.blue,
+              ],
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.lightBlueAccent.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 9,
+                offset: const Offset(0, 8), // changes position of shadow
+              ),
+            ],
+          ),
+          child: const Icon(
+            Icons.search,
+            size: 32.0,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 50.0,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: backGroundApp,
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.blueAccent,
+                offset: Offset(2, 2),
+                blurRadius: 6.0,
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                flex: 2,
+                child: columnX(
+                  context,
+                  Icons.home_outlined,
+                  0,
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: columnX(
+                  context,
+                  Icons.layers_outlined,
+                  1,
+                ),
+              ),
+              //Location Floating Action Button
+              const Expanded(
+                flex: 1,
+                child: SizedBox(width: 1),
+              ),
+              Expanded(
+                flex: 2,
+                child: columnX(
+                  context,
+                  Icons.credit_card_outlined,
+                  3,
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: columnX(
+                  context,
+                  Icons.settings_outlined,
+                  4,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+
+      body: Padding(
+        padding:  EdgeInsets.symmetric(horizontal: width*.05),
+        child: Column(
+          children: [
+            SizedBox(height: height*.05,),
+            Center(
+              child: Text(
+                'Here you will see notifications for applications',
+                style: TextStyle(
+                  fontFamily: 'Europa',
+                  fontSize: 14,
+                  color: const Color(0xffffffff),
+                  height: 1.5625,
+                ),
+                textHeightBehavior: TextHeightBehavior(applyHeightToFirstAscent: false),
+
+              ),
+            ),
+            SizedBox(height: height*.05,),
+            //Listview of Notifications
+            Expanded(
+              child: ListView.separated(
+                shrinkWrap: true,
+                itemCount: 4,
+                separatorBuilder:(context, index) =>  SizedBox(height: 10.0),
+                itemBuilder:(context, index) =>  Container(
+                  height: height*.07,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4.0),
+                    border: Border.all(width: 2.0, color: const Color(0xff198dbe)),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                            onPressed: (){},
+                            icon: Icon(Icons.notifications_outlined,color:  Color(0xff198dbe))),
+                        Text(
+                          'هذا مثال على اشعار مالوش أي ستين لازمه',
+                          style: TextStyle(
+                            fontFamily: 'FF Shamel Family Sans One',
+                            fontSize: 13,
+                            color: const Color(0xff198dbe),
+                            fontWeight: FontWeight.w300,
+                          ),
+                          textAlign: TextAlign.right,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  InkWell columnX(
+      context,
+      iconType,
+      int screenNumber,
+      ) {
+    return InkWell(
+      onTap: () {
+        // changeIndexScreen(context, screenNumber);
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(
+            iconType,
+            color: Colors.lightBlueAccent,
+            // color: getIndexScreen(context) == screenNumber
+            // : Colors.grey,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/subscribe.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Plans extends StatefulWidget {
+  const Plans({Key? key}) : super(key: key);
+
+  @override
+  _PlansState createState() => _PlansState();
+}
+
+class _PlansState extends State<Plans> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff19233E),
+      appBar: AppBar(
+        title: Text("Plans",style: TextStyle(color: Color(0xff39719F)),),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Color(0xff19233E),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(50.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+
+              Container(
+                height: 150.0,
+                child: Stack(
+                  children: [
+                    Image.asset("assets/images/bronze.png",fit: BoxFit.fill,height: 150),
+                    Positioned(
+                        top: 105.0,
+                        right: 70.0,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("500 view"),
+                            Text("1 No devices"),
+                          ],
+                        ))
+
+
+                  ],
+                ),
+              ),
+              Container(
+                height: 150.0,
+                child: Stack(
+                  children: [
+                    Image.asset("assets/images/silver.png",fit: BoxFit.fill,height: 150,),
+                    Positioned(
+                        top: 105.0,
+                        right: 70.0,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("1000 view"),
+                            Text("3 No devices"),
+                          ],
+                        ))
+
+                  ],
+                ),
+              ),
+              Container(
+                height: 150.0,
+                child: Stack(
+                  children: [
+                    Image.asset("assets/images/gold.png",fit: BoxFit.fill,height: 150,),
+                    Positioned(
+                        top: 105.0,
+                        right: 70.0,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("1000 view"),
+                            Text("3 No devices"),
+                          ],
+                        ))
+
+
+                  ],
+                ),
+              ),
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
